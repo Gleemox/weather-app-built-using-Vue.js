e14ee27cc5e56a42a4e50c5bcc4b1707990eecb8
Gleemox <ibuildsoftware@outlook.fr>
2025-07-31 12:14:21 +0100

    Setup API key to weather API

--- vue-weather/src/App.vue
+++ vue-weather/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  
+  <div id="app">
+  hello world!
+  </div>
 </template>
 
 <script>
-
-
-export default {
-  name: 'App',
+export default{
+name: 'app',
+data(){
+return {
+api_key: 'aa8eb439059f83b69f78c9d4ff7bc28e'
 }
+}
+}
+
 </script>
-
 <style>
-
 </style>
