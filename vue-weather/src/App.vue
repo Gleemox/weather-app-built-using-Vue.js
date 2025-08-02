394f24d6df0a8a16dc7bbc99bfb12485e86b0453
Gleemox <ibuildsoftware@outlook.fr>
2025-08-03 00:22:15 +0100

    Add HTML and CSS code to App.vue file

--- vue-weather/src/App.vue
+++ vue-weather/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
-  hello world!
+  <div class="search-box">
+  <input type="text" class="search-box" placeholder="Search ...."/>
   </div>
+  
+<main>
+
+</main>
+
+
+  
 </template>
 
 <script>
@@ -16,4 +24,16 @@ api_key: 'aa8eb439059f83b69f78c9d4ff7bc28e'
 
 </script>
 <style>
+*{
+margin:0;
+passing:0;
+box-sizing:border-box;
+}
+
+body{
+font-family:'montserrat',sans-serif;
+}
+
+#app{
+background-image:url('./assets/cold-bg.jpg');}
 </style>
